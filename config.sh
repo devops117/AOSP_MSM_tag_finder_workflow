87221a769faedfb3373ceae26f76842013220f17
devops117 <55235206+devops117@users.noreply.github.com>
2024-01-01 04:56:31 +0000

    use s2 kernel
    Signed-off-by: devops117 <55235206+devops117@users.noreply.github.com>

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-KERNEL_SOURCE=https://github.com/RahulPalXDA/kernel_samsung_j2y18lte.git
-BRANCH=stock
-KERNEL_VERSION=msm-3.18
-PLATFORM=*-89xx.0
+KERNEL_SOURCE=https://github.com/devops117/android-leeco-s2-kernel
+BRANCH=master
+KERNEL_VERSION=msm-3.10
+PLATFORM=*-8976.0
