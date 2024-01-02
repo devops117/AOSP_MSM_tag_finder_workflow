fd4e349eda12ab9c762e03a4abb1733d81cdd250
devops117 <55235206+devops117@users.noreply.github.com>
2024-01-03 01:30:47 +0530

    export the environ-vars
    Signed-off-by: devops117 <55235206+devops117@users.noreply.github.com>

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-KERNEL_SOURCE=https://github.com/devops117/android-leeco-s2-kernel
-BRANCH=master
-KERNEL_VERSION=msm-3.10
-PLATFORM=*-8976.0
+export KERNEL_SOURCE=https://github.com/devops117/android-leeco-s2-kernel
+export BRANCH=master
+export KERNEL_VERSION=msm-3.10
+export PLATFORM=*-8976.0
